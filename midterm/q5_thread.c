c1ca73708eeeecc6dbc3d5918a1356833a8caf43
teacher144123 <teacher144123@gmail.com>
2020-04-27 03:21:21 +0800

    modify thread numbers

--- midterm/q5_thread.c
+++ midterm/q5_thread.c
@@ -4,8 +4,11 @@
 #include <sys/time.h>
 #include <pthread.h>
 
-int test_times = 50;
+int total_times = 50;
 int test_size = 100;
+int npes = 25; // threads number
+// count how many task be assigned to one core
+int test_times = 2;
 
 float results[50];
 
@@ -22,17 +25,22 @@ float updateTimer(struct timeval *time_start, struct timeval *time_end) {
 }
 void *count(void *arg) {
 	int now = *(int*)arg;
-	int i;
-	float temp = 0;
+	int i, j;
+    float temp[test_times];
 
-	for (i = 0; i < test_size; i++) {
-		temp += (float)(rand()) / RAND_MAX;
-	}
-	temp /= (float)test_size;
+    for (i = 0;i < test_times;++i) {
+        temp[i] = 0;
+        for (j = 0; j < test_size; j++) {
+        	temp[i] += (float)(rand()) / RAND_MAX;
+        }
+    	temp[i] /= (float)test_size;
 
-	printf("%f\n", temp);
+        int k = now * test_times + i;
+        if (k < total_times) {
+            results[k] = temp[i];
+        }
+    }
 
-	results[now] = temp;
 	return NULL;
 }
 int main(int argc, char const *argv[]) {
@@ -43,21 +51,24 @@ int main(int argc, char const *argv[]) {
 	float t_temp; // save flaot time
     gettimeofday(&time_start, NULL);
 
+    test_times = (int)(total_times / npes);
+    test_times = total_times % npes ? test_times + 1 : test_times;
+
 	// Parameters
 	int i;
-	pthread_t task_ids[test_times];
+	pthread_t task_ids[npes];
 
 	putchar('c');
-	for (i = 0; i < test_times; i++) {
+	for (i = 0; i < npes; i++) {
 		// start all threads running and save p_id into task_ids
 		pthread_create(&(task_ids[i]), NULL, (void *)count, (void *)&i);
 	}
-	for (i = 0; i < test_times; i++) {
+	for (i = 0; i < npes; i++) {
 		// waiting all tasks return
 		pthread_join(task_ids[i], NULL);
 	}
 
-	// for (i = 0; i < test_times; i++) {
+	// for (i = 0; i < total_times; i++) {
 	// 	printf("%d %f\n", i, results[i]);
 	// }
 
