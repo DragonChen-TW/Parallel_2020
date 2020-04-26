94b50fa597892d186a620ddedda882379341661d
teacher144123 <teacher144123@gmail.com>
2020-04-27 00:50:03 +0800

    modify c_mpi.c

--- midterm/q5_mpi.c
+++ midterm/q5_mpi.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+// #include <time.h>
+#include <sys/time.h>
 #include <mpi.h>
+
+float updateTimer(struct timeval *time_start, struct timeval *time_end) {
+    float timeuse;
+
+    gettimeofday(time_end, NULL);
+    timeuse = 1000000*(time_end->tv_sec - time_start->tv_sec)
+        + time_end->tv_usec - time_start->tv_usec;
+    timeuse /= 1000000;
+
+    *time_start = *time_end;
+    return timeuse;
+}
 
 int main(int argc, char const *argv[]) {
 	int my_rank;
 	MPI_Status status;
 
 	// init
+	struct timeval time_start, time_end;
+	float t_temp; // save flaot time
+    gettimeofday(&time_start, NULL);
+
 	MPI_Init(&argc, (char***)&argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
@@ -32,6 +49,9 @@ int main(int argc, char const *argv[]) {
 		for (i = 0; i < test_times; i++) {
 			printf("%d %f\n", i, results[i]);
 		}
+
+		t_temp = updateTimer(&time_start, &time_end);
+		printf("Cost: %.4f secs.\n", t_temp);
 	} else {
 		MPI_Gather(&temp, 1, MPI_FLOAT, NULL, 0, MPI_FLOAT, 0, MPI_COMM_WORLD);
 	}
